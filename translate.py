2d4559bd3f9dd4bcffa2dfa3c3d10a1cd5887a97
Ali Akbar Septiandri <ali.septiandri@gmail.com>
2023-09-17 11:34:08 +0100

    Change dir and add flan-t5

--- translate.py
+++ translate.py
@@ -1,5 +1,7 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from tqdm import trange
 
+import click
 import dotenv
 import os
 
@@ -9,6 +11,15 @@ import requests
 
 dotenv.load_dotenv()
 
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
+
+def translate(texts):
+    text_batch = [f"Translate from French to English: {text}" for text in texts]
+    inputs = tokenizer(text_batch, padding=True, truncation=True, return_tensors="pt")
+    outputs = model.generate(**inputs, max_length=1024)
+    return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
 def deepl_translate(texts):
     resp = requests.post(
         'https://api-free.deepl.com/v2/translate',
@@ -17,15 +28,24 @@ def deepl_translate(texts):
     )
     return [item['text'] for item in resp.json()['translations']]
 
-if __name__ == "__main__":
-    batch_size = 50
+@click.command
+@click.option('-a', '--api', is_flag=True)
+@click.option('--batch_size', default=50)
+def main(api, batch_size):
+    print("Loading data...")
     df_fr = pd.read_json('ML-ESG-2_French_Train.json')
     n_iters = df_fr.shape[0] // batch_size + 1
 
     translated_texts = []
     for i in trange(n_iters):
         try:
-            results = deepl_translate(df_fr['news_content'].iloc[i*batch_size:(i+1)*batch_size].values.tolist())
+            texts = df_fr['news_content'].iloc[i*batch_size:(i+1)*batch_size].values.tolist()
+
+            if api:
+                results = deepl_translate(texts)
+            else:
+                results = translate(texts)
+
             translated_texts.append(results)
         except:
             print(f"Error at batch {i}")
@@ -36,3 +56,6 @@ if __name__ == "__main__":
     })
 
     df.to_csv('fr_en.csv', index=False)
+
+if __name__ == "__main__":
+    main()
